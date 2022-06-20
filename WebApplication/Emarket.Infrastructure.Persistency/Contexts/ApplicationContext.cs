7a12dd79315745ced6bb5b6df6efec236d3ef497
Edwin Roman <romanseberino@gmail.com>
2022-06-19 23:12:22 -0400

    Onion Clean Architecture Setup and Dependecies to use through the work.

---
+++ WebApplication/Emarket.Infrastructure.Persistency/Contexts/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emarket.Infrastructure.Persistency.Contexts
+{
+    public class ApplicationContext
+    {
+    }
+}
